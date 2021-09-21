dac9c96ec3c24dfa70991e1d9604920e046663e8
fitse-yotor <fitseyoto@gmail.com>
2021-09-21 15:35:13 +0300

    catagories

---
+++ lib/Widget/home_widgets/Catagories.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Catgories extends StatefulWidget {
+  Catgories({Key? key}) : super(key: key);
+
+  @override
+  _CatgoriesState createState() => _CatgoriesState();
+}
+
+class _CatgoriesState extends State<Catgories> {
+  bool isAll = true;
+  bool isFashion = false;
+  bool isClasic = false;
+  bool isLocal = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
