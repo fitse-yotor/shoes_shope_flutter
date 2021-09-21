719794ecbd065eaab23acd00f614fa93e607f176
fitse-yotor <fitseyoto@gmail.com>
2021-09-21 16:27:13 +0300

    serchbar

--- lib/Widget/search_bar.dart
+++ lib/Widget/search_bar.dart
@@ -5,32 +5,47 @@ import 'package:shoes/constants/app_constants.dart';
 class SearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: const EdgeInsets.symmetric(horizontal: 12),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Container(
-          height: 50,
-                    width: 270,
-                    padding: EdgeInsets.symmetric(horizontal: 15),
-                    decoration: BoxDecoration(
-                      color: kAccentColor,
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Search Products...",
-                          style: TextStyle(
-                              color: Colors.white, fontFamily: "Poppins-Light"),
-                        ),
-                        kSearchIcon,
-                      ],
-                    )
-        )
-      ],
-    ),);
-    
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            height: 50,
+            width: 270,
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            decoration: BoxDecoration(
+              color: kAccentColor,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Search Products...",
+                  style: TextStyle(
+                      color: Colors.white, fontFamily: "Poppins-Light"),
+                ),
+                kSearchIcon,
+              ],
+            ),
+          ),
+          SizedBox(
+            width: 6,
+          ),
+          Container(
+            height: 50,
+            width: 48,
+            decoration: BoxDecoration(
+              color: kAccentColor,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Center(
+              child: kSortIcon,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
