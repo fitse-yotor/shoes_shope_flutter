4c660c97c5d8bdc68f9e483db79dc2b659c24385
fitse-yotor <fitseyoto@gmail.com>
2021-09-21 15:18:50 +0300

    some fix

--- lib/screen/Home.dart
+++ lib/screen/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoes/constants/animation/customAnimation.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -10,8 +11,12 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 4, child: CustomAnimation)
-      
-    
+    return DefaultTabController(
+        length: 4,
+        child: CustomAnimation(
+          home: Column(
+            children: [Text('kdfkjnj')],
+          ),
+        ));
   }
 }
