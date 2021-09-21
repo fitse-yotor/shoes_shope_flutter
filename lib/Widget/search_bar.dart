2b2606e7e3efe8aa30a83646f1f8aa7fc4d88ff7
fitse-yotor <fitseyoto@gmail.com>
2021-09-21 16:23:42 +0300

    serach bar

---
+++ lib/Widget/search_bar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(padding: const EdgeInsets.symmetric(horizontal: 12),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          
+        )
+      ],
+    ),);
+    
+  }
+}
